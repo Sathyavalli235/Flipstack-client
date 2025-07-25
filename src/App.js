e8e2f3d059b5eb7545994607f860756e9c4af24e
Sathyavalli235 <sathyavalli235@gmail.com>
2025-07-25 12:23:55 +0530

    update message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import Signup from './Signup';
